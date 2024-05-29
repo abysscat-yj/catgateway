e356830a3ac0897b91b0e011b14ae17c433aacf9
abysscat-yj <397967552@qq.com>
2024-05-30 02:07:49 +0800

    init

---
+++ src/main/java/com/abysscat/catgateway/CatgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.abysscat.catgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatgatewayApplication.class, args);
+	}
+
+}
